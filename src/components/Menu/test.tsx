1b2668db4abdebd0ec42dfe61cbf1e4a6e8ee003
GusttavoCastilho <gustavocvalenciano@gmail.com>
2021-06-28 21:26:39 -0300

    create component menu

---
+++ src/components/Menu/test.tsx
@@ -0,0 +1,50 @@
+import { fireEvent, screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import Menu from '.'
+
+describe('<Menu />', () => {
+  it('should render the menu', () => {
+    renderWithTheme(<Menu />)
+
+    expect(screen.getByLabelText(/open menu/i)).toBeInTheDocument()
+    expect(screen.getByLabelText(/search/i)).toBeInTheDocument()
+    expect(screen.getByLabelText(/open shopping cart/i)).toBeInTheDocument()
+    expect(screen.getByRole('img', { name: /won games/i })).toBeInTheDocument()
+  })
+
+  it('should render the open/close mobile menu', () => {
+    renderWithTheme(<Menu />)
+
+    const fullElement = screen.getByRole('navigation', { hidden: true })
+
+    expect(fullElement.getAttribute('aria-hidden')).toBe('true')
+    expect(fullElement).toHaveStyle({ opacity: 0 })
+
+    fireEvent.click(screen.getByLabelText(/open menu/i))
+    expect(fullElement.getAttribute('aria-hidden')).toBe('false')
+    expect(fullElement).toHaveStyle({ opacity: 1 })
+
+    fireEvent.click(screen.getByLabelText(/close menu/i))
+    expect(fullElement.getAttribute('aria-hidden')).toBe('true')
+    expect(fullElement).toHaveStyle({ opacity: 0 })
+  })
+
+  it('should show register box when logged out', () => {
+    renderWithTheme(<Menu />)
+
+    expect(screen.getByText(/log in now/i)).toBeInTheDocument()
+    expect(screen.getByText(/sign up/i)).toBeInTheDocument()
+    expect(screen.queryByText(/my account/i)).not.toBeInTheDocument()
+    expect(screen.queryByText(/wishlist/i)).not.toBeInTheDocument()
+  })
+
+  it('should show wishlist and account when logged in', () => {
+    renderWithTheme(<Menu username="Gustavo" />)
+
+    expect(screen.queryByText(/log in now/i)).not.toBeInTheDocument()
+    expect(screen.queryByText(/sign up/i)).not.toBeInTheDocument()
+    expect(screen.getByText(/my account/i)).toBeInTheDocument()
+    expect(screen.getByText(/wishlist/i)).toBeInTheDocument()
+  })
+})
