b146d23571d7e95fa592813f10e216d47be1fc34
GusttavoCastilho <gustavocvalenciano@gmail.com>
2021-06-30 21:50:32 -0300

    add prop AS from component button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,13 +1,17 @@
-import React from 'react'
+import React, { AnchorHTMLAttributes } from 'react'
+import { ButtonHTMLAttributes } from 'react'
 import * as S from './styles'
 
+type ButtonTypes =
+  | AnchorHTMLAttributes<HTMLAnchorElement>
+  | ButtonHTMLAttributes<HTMLButtonElement>
+
 export type ButtonProps = {
-  children?: React.ReactNode
   size?: 'large' | 'medium' | 'small'
   fullWidth?: boolean
   icon?: React.ReactNode
-  onClick?: () => (event: React.MouseEvent<HTMLButtonElement>) => void
-}
+  as?: React.ElementType
+} & ButtonTypes
 
 const Button = ({
   children,
