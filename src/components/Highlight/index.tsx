aa5bc075533b90c63e3cde5504c2632b17b69b0a
GusttavoCastilho <gustavocvalenciano@gmail.com>
2021-09-22 12:37:19 -0300

    feat: Add images and created struct to component Highlight

---
+++ src/components/Highlight/index.tsx
@@ -0,0 +1,26 @@
+import * as S from './styles'
+import Button from 'components/Button'
+
+export type HighlightProps = {
+  title: string
+  subtitle: string
+  buttonLabel: string
+  buttonLink: string
+}
+
+const Highlight = ({
+  title,
+  subtitle,
+  buttonLabel,
+  buttonLink
+}: HighlightProps) => (
+  <S.Wrapper>
+    <S.Title>{title}</S.Title>
+    <S.Subtitle>{subtitle}</S.Subtitle>
+    <Button as="a" href={buttonLink}>
+      {buttonLabel}
+    </Button>
+  </S.Wrapper>
+)
+
+export default Highlight
