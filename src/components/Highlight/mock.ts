8cd15c3ce725a99b93a1ae9861f555cbb2d04bae
GusttavoCastilho <gustavocvalenciano@gmail.com>
2021-09-26 17:55:55 -0300

    feat: Created mock and connecting the data to the page ✨

---
+++ src/components/Highlight/mock.ts
@@ -0,0 +1,7 @@
+export default {
+  title: 'Read Dead is Back!',
+  subtitle: 'Come see John´s new adventures',
+  buttonLabel: 'Buy now',
+  buttonLink: '/rdr2',
+  backgroundImage: '/img/red-dead-img.png'
+}
--- src/components/Menu/styles.ts
+++ src/components/Menu/styles.ts
@@ -92,7 +92,8 @@ export const MenuFull = styled.nav<MenuFullProps>`
     flex-direction: column;
     justify-content: space-between;
     background-color: ${theme.colors.white};
-    position: absolute;
+    position: fixed;
+    z-index: ${theme.layers.menu};
     top: 0;
     bottom: 0;
     right: 0;
