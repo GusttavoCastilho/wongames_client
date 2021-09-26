1b8c6620a298f2e41ad25c1da324ad2e818ac6d6
GusttavoCastilho <gustavocvalenciano@gmail.com>
2021-09-26 18:07:53 -0300

    chore: optimize tests from home page 🔧

--- src/templates/Home/test.tsx
+++ src/templates/Home/test.tsx
@@ -10,13 +10,13 @@ import highlightMock from 'components/Highlight/mock'
 
 const props = {
   banners: bannersMock,
-  newGames: gamesMock,
+  newGames: [gamesMock[0]],
   mostPopularHighlight: highlightMock,
-  mostPopularGames: gamesMock,
-  upcommingGames: gamesMock,
+  mostPopularGames: [gamesMock[0]],
+  upcommingGames: [gamesMock[0]],
   upcommingHighlight: highlightMock,
-  upcommingMoreGames: gamesMock,
-  freeGames: gamesMock,
+  upcommingMoreGames: [gamesMock[0]],
+  freeGames: [gamesMock[0]],
   freeHighlight: highlightMock
 }
 
@@ -29,29 +29,27 @@ describe('<Home />', () => {
     expect(
       screen.getByRole('heading', { name: /Contact us/i })
     ).toBeInTheDocument()
-  })
-
-  it('should render the sections', () => {
-    renderWithTheme(<Home {...props} />)
 
     expect(screen.getByRole('heading', { name: /News/i })).toBeInTheDocument()
+
     expect(
       screen.getByRole('heading', { name: /Most Popular/i })
     ).toBeInTheDocument()
+
     expect(
       screen.getByRole('heading', { name: /Upcomming/i })
     ).toBeInTheDocument()
+
     expect(
       screen.getByRole('heading', { name: /Free Games/i })
     ).toBeInTheDocument()
-  })
 
-  it('should render section elements', () => {
-    renderWithTheme(<Home {...props} />)
     // banner
     expect(screen.getAllByText(/defy death 1/i)).toHaveLength(1)
-    // card game ( 5 sections com 4 cards cada = 5x4 = 20)
-    expect(screen.getAllByText(/population zero/i)).toHaveLength(20)
+
+    // card game ( 5 sections com 1 cards cada = 5x1 = 5)
+    expect(screen.getAllByText(/population zero/i)).toHaveLength(5)
+
     // highlight
     expect(screen.getAllByText(/read dead is back!/i)).toHaveLength(3)
   })
