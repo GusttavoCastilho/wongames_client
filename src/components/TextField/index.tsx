d3887c2e1cc42e130a20a8f1f1576851f930731a
GusttavoCastilho <gustavocvalenciano@gmail.com>
2021-10-02 17:16:08 -0300

    feat: Create struct initial from component and add icon

---
+++ src/components/TextField/index.tsx
@@ -0,0 +1,41 @@
+import { useState, InputHTMLAttributes } from 'react'
+
+import * as S from './styles'
+
+export type TextFieldProps = {
+  onInput?: (value: string) => void
+  label?: string
+  labelFor?: string
+  initialValue?: string
+  icon?: React.ReactNode
+} & InputHTMLAttributes<HTMLInputElement>
+
+const TextField = ({
+  label,
+  labelFor = '',
+  initialValue = '',
+  onInput,
+  icon,
+  ...props
+}: TextFieldProps) => {
+  const [value, setValue] = useState(initialValue)
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newValue = e.currentTarget.value
+    setValue(newValue)
+
+    !!onInput && onInput(newValue)
+  }
+
+  return (
+    <S.Wrapper>
+      {!!label && <S.Label htmlFor={labelFor}>{label}</S.Label>}
+      <S.InputWrapper>
+        {!!icon && <S.Icon>{icon}</S.Icon>}
+        <S.Input type="text" onChange={onChange} value={value} {...props} />
+      </S.InputWrapper>
+    </S.Wrapper>
+  )
+}
+
+export default TextField
