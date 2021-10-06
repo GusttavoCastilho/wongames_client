d52022fd5fa7b6d3d9daabfbfc20631b73da8c26
GusttavoCastilho <gustavocvalenciano@gmail.com>
2021-10-06 18:11:44 -0300

    style: Create style from template 💎

--- src/templates/Auth/index.tsx
+++ src/templates/Auth/index.tsx
@@ -10,20 +10,27 @@ type AuthProps = {
 const Auth = ({ title, children }: AuthProps) => (
   <S.Wrapper>
     <S.BannerBlock>
-      <Logo />
+      <S.BannerContent>
+        <Logo />
 
-      <Heading>All your favorite games in one place</Heading>
-      <S.Subtitle>
-        <strong>WON</strong> is the best and most complete gamin platform.
-      </S.Subtitle>
-      <S.Footer>Won Games 2020 © Todos os Direitos Reservados</S.Footer>
+        <div>
+          <Heading>All your favorite games in one place</Heading>
+          <S.Subtitle>
+            <strong>WON</strong> is the best and most complete gamin platform.
+          </S.Subtitle>
+        </div>
+
+        <S.Footer>Won Games 2020 © Todos os Direitos Reservados</S.Footer>
+      </S.BannerContent>
     </S.BannerBlock>
     <S.Content>
-      <Logo color="black" size="large" />
-      <Heading color="black" lineLeft lineColor="secondary">
-        {title}
-      </Heading>
-      {children}
+      <S.ContentWrapper>
+        <Logo color="black" size="large" />
+        <Heading color="black" lineLeft lineColor="secondary">
+          {title}
+        </Heading>
+        {children}
+      </S.ContentWrapper>
     </S.Content>
   </S.Wrapper>
 )
