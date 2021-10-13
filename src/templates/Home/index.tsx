5537e96fafbac1bd07f952093bb4c6428ea7600c
GusttavoCastilho <gustavocvalenciano@gmail.com>
2021-10-13 12:58:22 -0300

    feat: Create base template ✨

--- src/templates/Home/index.tsx
+++ src/templates/Home/index.tsx
@@ -1,13 +1,13 @@
-import { Container } from 'components/Container'
-import Footer from 'components/Footer'
-import Menu from 'components/Menu'
+import Base from 'templates/Base'
 
+import { Container } from 'components/Container'
 import { BannerProps } from 'components/Banner'
 import { GameCardProps } from 'components/GameCard'
 import { HighlightProps } from 'components/Highlight'
 import BannerSlider from 'components/BannerSlider'
-import * as S from './styles'
 import Showcase from 'components/Showcase'
+
+import * as S from './styles'
 
 export type HomeTemplateProps = {
   banners: BannerProps[]
@@ -32,10 +32,8 @@ const Home = ({
   freeGames,
   freeHighlight
 }: HomeTemplateProps) => (
-  <section>
+  <Base>
     <Container>
-      <Menu />
-
       <S.SectionBanner>
         <BannerSlider items={banners} />
       </S.SectionBanner>
@@ -57,13 +55,7 @@ const Home = ({
     </S.SectionUpcoming>
 
     <Showcase title="Free games" highlight={freeHighlight} games={freeGames} />
-
-    <S.SectionFooter>
-      <Container>
-        <Footer />
-      </Container>
-    </S.SectionFooter>
-  </section>
+  </Base>
 )
 
 export default Home
