89317f5bf4423514af6a45786793013ace55b264
GusttavoCastilho <gustavocvalenciano@gmail.com>
2021-10-14 17:52:22 -0300

    feat: add modal and created tests ✨

--- src/components/Gallery/test.tsx
+++ src/components/Gallery/test.tsx
@@ -1,19 +1,50 @@
-import { screen } from '@testing-library/react'
+import 'match-media-mock'
+import { screen, fireEvent } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helpers'
 
 import Gallery from '.'
 
-const props = {
-  items: [
-    {
-      src: '/img/games/cyberpunk-1.jpg',
-      label: 'Cyberpunk'
-    }
-  ]
-}
+import mockItems from './mock'
 
 describe('<Gallery />', () => {
-  it('should render the heading', () => {
-    renderWithTheme(<Gallery {...props} />)
+  it('should render the thumbnails as buttons', () => {
+    renderWithTheme(<Gallery items={mockItems.slice(0, 2)} />)
+
+    expect(
+      screen.getByRole('button', { name: /Thumb - Gallery Image 1/i })
+    ).toHaveAttribute('src', mockItems[0].src)
+
+    expect(
+      screen.getByRole('button', { name: /Thumb - Gallery Image 2/i })
+    ).toHaveAttribute('src', mockItems[1].src)
+  })
+
+  it('should handle open modal', () => {
+    renderWithTheme(<Gallery items={mockItems.slice(0, 2)} />)
+
+    const modal = screen.getByLabelText('modal')
+
+    expect(modal.getAttribute('aria-hidden')).toBe('true')
+    expect(modal).toHaveStyle({ opacity: 0 })
+
+    fireEvent.click(
+      screen.getByRole('button', { name: /Thumb - Gallery Image 1/i })
+    )
+    expect(modal.getAttribute('aria-hidden')).toBe('false')
+    expect(modal).toHaveStyle({ opacity: 1 })
+  })
+
+  it('should handle close modal when overlay or button clicked', () => {
+    renderWithTheme(<Gallery items={mockItems.slice(0, 2)} />)
+
+    const modal = screen.getByLabelText('modal')
+
+    fireEvent.click(
+      screen.getByRole('button', { name: /Thumb - Gallery Image 1/i })
+    )
+
+    fireEvent.click(screen.getByRole('button', { name: /close modal/i }))
+    expect(modal.getAttribute('aria-hidden')).toBe('true')
+    expect(modal).toHaveStyle({ opacity: 0 })
   })
 })
