9904cb8c5ee60f4a4ebce1a6279f3b25dbb23fea
GusttavoCastilho <gustavocvalenciano@gmail.com>
2021-10-14 18:29:25 -0300

    feat: Create ref to slider ✨

--- src/components/Gallery/test.tsx
+++ src/components/Gallery/test.tsx
@@ -34,6 +34,17 @@ describe('<Gallery />', () => {
     expect(modal).toHaveStyle({ opacity: 1 })
   })
 
+  it('should open modal with selected image', async () => {
+    renderWithTheme(<Gallery items={mockItems.slice(0, 2)} />)
+
+    fireEvent.click(
+      screen.getByRole('button', { name: /Thumb - Gallery Image 2/i })
+    )
+
+    const img = await screen.findByRole('img', { name: /Gallery Image 2/i })
+    expect(img.parentElement?.parentElement).toHaveClass('slick-active')
+  })
+
   it('should handle close modal when overlay or button clicked', () => {
     renderWithTheme(<Gallery items={mockItems.slice(0, 2)} />)
 
