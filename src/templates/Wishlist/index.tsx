0fd53d17bee41e3dd8cb615dfe85cf1aea309c5c
GusttavoCastilho <gustavocvalenciano@gmail.com>
2021-10-26 10:46:50 -0300

    feat: add games from page Wishlist ✨

--- src/templates/Wishlist/index.tsx
+++ src/templates/Wishlist/index.tsx
@@ -2,18 +2,20 @@ import Base from 'templates/Base'
 
 import Heading from 'components/Heading'
 import { Container } from 'components/Container'
-import { GameCardProps } from 'components/GameCard'
+import GameCard, { GameCardProps } from 'components/GameCard'
 import { HighlightProps } from 'components/Highlight'
 
 import * as S from './styles'
 import Showcase from 'components/Showcase'
 
 export type WishlistTemplateProps = {
+  games?: GameCardProps[]
   recommendedGames: GameCardProps[]
   recommendedHighlight: HighlightProps
 }
 
 const Wishlist = ({
+  games,
   recommendedGames,
   recommendedHighlight
 }: WishlistTemplateProps) => (
@@ -22,6 +24,10 @@ const Wishlist = ({
       <Heading lineLeft lineColor="secondary">
         Wishlist
       </Heading>
+
+      {games?.map((game, index) => (
+        <GameCard key={`wishlist-${index}`} {...game} />
+      ))}
     </Container>
 
     <Showcase
--- src/templates/Wishlist/test.tsx
+++ src/templates/Wishlist/test.tsx
@@ -1,13 +1,35 @@
-import { render, screen } from '@testing-library/react'
+import 'match-media-mock'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
 
 import Wishlist from '.'
 
+import gamesMock from 'components/GameCardSlider/mock'
+import highlightMock from 'components/Highlight/mock'
+
+const props = {
+  games: gamesMock,
+  recommendedHighlight: highlightMock,
+  recommendedGames: gamesMock
+}
+
+jest.mock('components/Showcase', () => ({
+  __esModule: true,
+  default: function Mock() {
+    return <div data-testid="Mock Showcase" />
+  }
+}))
+
 describe('<Wishlist />', () => {
   it('should render the heading', () => {
-    const { container } = render(<Wishlist />)
+    renderWithTheme(<Wishlist {...props} />)
 
-    expect(screen.getByRole('heading', { name: /Wishlist/i })).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /Wishlist/i })
+    ).toBeInTheDocument()
 
-    expect(container.firstChild).toMatchSnapshot()
+    expect(screen.getAllByText(/population zero/i)).toHaveLength(6)
+
+    expect(screen.getByTestId('Mock Showcase')).toBeInTheDocument()
   })
 })
