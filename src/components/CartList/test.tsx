f63baef467871ef2765d555000b2eac4bed67697
GusttavoCastilho <gustavocvalenciano@gmail.com>
2021-11-03 12:18:38 -0300

    feat: add option of button link ✨

--- src/components/CartList/test.tsx
+++ src/components/CartList/test.tsx
@@ -15,4 +15,10 @@ describe('<CartList />', () => {
 
     expect(container.firstChild).toMatchSnapshot()
   })
+
+  it('should render the button', () => {
+    renderWithTheme(<CartList items={itemsMock} total="R$ 330,00" hasButton />)
+
+    expect(screen.getByText(/buy it now/i)).toBeInTheDocument()
+  })
 })
